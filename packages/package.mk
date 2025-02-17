3f280ad3969c41d8bbe54b63c5c4b81295598105
jeck.chen <jeck.chen@dbappsecurity.com.cn>
2025-02-17 09:36:08 +0800

    fix binutils link to system zlib bug
    Signed-off-by: jeck.chen <jeck.chen@dbappsecurity.com.cn>

--- packages/package.mk
+++ packages/package.mk
@@ -71,10 +71,8 @@ ifeq ($(STRIP_ALL),y)
 	-find $(FINAL_OUTPUT_DIR)/$(TARGET)/$(SYSROOT_NAME)/sbin | xargs $(TOOLCHAIN_BIN_DIR)/$(TARGET)-strip
 	-find $(FINAL_OUTPUT_DIR)/$(TARGET)/$(SYSROOT_NAME)/libexec | xargs $(TOOLCHAIN_BIN_DIR)/$(TARGET)-strip
 endif
-	@$(TOP_DIR)/symlinkconv.sh "$(FINAL_OUTPUT_DIR)"
 	@find $(FINAL_OUTPUT_DIR) -name '*.la' | xargs sed -i -e 's/-L$(subst /,\/,$(HOST_OUTPUT_PREFIX))\/lib//g' -e 's/$(subst /,\/,$(INSTALL_DIR))//g'
-	@cat $(FINAL_SYSROOT_DIR)/lib/libc.so | sed 's|$(TARGET_OUTPUT_DIR)/target||g' | xargs echo > libc.so.tmp
-	mv libc.so.tmp $(FINAL_SYSROOT_DIR)/lib/libc.so
+	@sed -i 's|$(TARGET_OUTPUT_DIR)/target||g' $(FINAL_SYSROOT_DIR)/lib/libc.so
 endif
 
 dir-prep:
